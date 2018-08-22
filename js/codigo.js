c7f9278c69008770e058a33c4917fae99f286a21
guadalupeaguilar <guadalupeaguilarmedina@outlook.com>
2018-08-22 13:41:43 -0500

    js

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+alert("INCLUSION") //primer alert
+
+//codigo de Hola a todos
+function Saludar(nombre){
+document.write(`"Hola ${nombre}`)
+}
+Saludar("a todos");
+
+alert("soy el primer script")//segundo alert
---
+++ js/codigofun.js
@@ -0,0 +1,19 @@
+alert("Función suma de 4 y 5")
+function Suma(numero1,numero2){
+return numero1 + numero2}
+alert(Suma(5,4));
+
+alert("Funcion resta de 4 y 5")
+function resta(numero1,numero2){
+return numero1 - numero2}
+alert(resta(5,4));
+
+alert("Funcion multiplicacion de 4 y 5")
+function multi(numero1,numero2){
+return numero1 * numero2}
+alert(multi(5,4));
+
+alert("Funcion division de 4 y 5")
+function divi(numero1,numero2){
+return numero1 / numero2}
+alert(divi(5,4));
